8916eb470f9895c842b0058670da7ce0817ca876
Daniel Armond <dani.xavierarmond@gmail.com>
2023-01-06 17:08:04 -0300

    Aula3

---
+++ Aula2/Aula2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula2 { 
+class Program {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome completo: ");
+            string nome = Console.ReadLine();
+            string[] nomes = nome.Split(" ");
+            string primeiro = nomes[0];
+            string segundo = nomes[1];
+            string terceiro = nomes[2];
+
+            Console.WriteLine($"Seu primeiro nome é: {primeiro}");
+            Console.WriteLine($"Seu segundo nome é: {segundo}");
+            Console.WriteLine($"Seu terceiro nome é: {terceiro}");
+
+
+        }
+
+
+
+    }
+
+
+
+}
+
