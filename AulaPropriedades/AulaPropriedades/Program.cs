f421e15d2e774bf45736e39e4f48a316bac069b5
Daniel Armond <dani.xavierarmond@gmail.com>
2023-01-09 18:20:39 -0300

    Aula3

---
+++ AulaPropriedades/AulaPropriedades/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AulaPropriedades {
+
+    class Program {
+
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.Nome = "T";
+
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+
+
+
+
+
+
+
+        }
+    }
+}
